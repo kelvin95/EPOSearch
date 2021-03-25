be4994c966fe1d8089d46c28cca3b5faab592d79
Amanjit <amanjitsk@cs.toronto.edu>
2021-03-25 01:43:41 -0400

    cleanup epo_lp

--- multiMNIST/solvers/epo_lp.py
+++ multiMNIST/solvers/epo_lp.py
@@ -4,7 +4,6 @@ import cvxopt
 
 
 class EPO_LP(object):
-
     def __init__(self, m, n, r, eps=1e-4):
         cvxopt.glpk.options["msg_lev"] = "GLP_MSG_OFF"
         self.m = m
@@ -12,29 +11,38 @@ class EPO_LP(object):
         self.r = r
         self.eps = eps
         self.last_move = None
-        self.a = cp.Parameter(m)        # Adjustments
-        self.C = cp.Parameter((m, m))   # C: Gradient inner products, G^T G
-        self.Ca = cp.Parameter(m)       # d_bal^TG
-        self.rhs = cp.Parameter(m)      # RHS of constraints for balancing
+        self.a = cp.Parameter(m)  # Adjustments
+        self.C = cp.Parameter((m, m))  # C: Gradient inner products, G^T G
+        self.Ca = cp.Parameter(m)  # d_bal^TG
+        self.rhs = cp.Parameter(m)  # RHS of constraints for balancing
 
-        self.alpha = cp.Variable(m)     # Variable to optimize
+        self.alpha = cp.Variable(m)  # Variable to optimize
 
-        obj_bal = cp.Maximize(self.alpha @ self.Ca)   # objective for balance
-        constraints_bal = [self.alpha >= 0, cp.sum(self.alpha) == 1,  # Simplex
-                           self.C @ self.alpha >= self.rhs]
+        obj_bal = cp.Maximize(self.alpha @ self.Ca)  # objective for balance
+        constraints_bal = [
+            self.alpha >= 0,
+            cp.sum(self.alpha) == 1,  # Simplex
+            self.C @ self.alpha >= self.rhs,
+        ]
         self.prob_bal = cp.Problem(obj_bal, constraints_bal)  # LP balance
 
         obj_dom = cp.Maximize(cp.sum(self.alpha @ self.C))  # obj for descent
-        constraints_res = [self.alpha >= 0, cp.sum(self.alpha) == 1,  # Restrict
-                           self.alpha @ self.Ca >= -cp.neg(cp.max(self.Ca)),
-                           self.C @ self.alpha >= 0]
-        constraints_rel = [self.alpha >= 0, cp.sum(self.alpha) == 1,  # Relaxed
-                           self.C @ self.alpha >= 0]
+        constraints_res = [
+            self.alpha >= 0,
+            cp.sum(self.alpha) == 1,  # Restrict
+            self.alpha @ self.Ca >= -cp.neg(cp.max(self.Ca)),
+            self.C @ self.alpha >= 0,
+        ]
+        constraints_rel = [
+            self.alpha >= 0,
+            cp.sum(self.alpha) == 1,  # Relaxed
+            self.C @ self.alpha >= 0,
+        ]
         self.prob_dom = cp.Problem(obj_dom, constraints_res)  # LP dominance
         self.prob_rel = cp.Problem(obj_dom, constraints_rel)  # LP dominance
 
-        self.gamma = 0     # Stores the latest Optimum value of the LP problem
-        self.mu_rl = 0     # Stores the latest non-uniformity
+        self.gamma = 0  # Stores the latest Optimum value of the LP problem
+        self.mu_rl = 0  # Stores the latest non-uniformity
 
     def get_alpha(self, l, G, r=None, C=False, relax=False):
         r = self.r if r is None else r
@@ -48,7 +56,7 @@ class EPO_LP(object):
             if len(np.where(J)[0]) > 0:
                 J_star_idx = np.where(rl == np.max(rl))[0]
                 self.rhs.value = self.Ca.value.copy()
-                self.rhs.value[J] = -np.inf     # Not efficient; but works.
+                self.rhs.value[J] = -np.inf  # Not efficient; but works.
                 self.rhs.value[J_star_idx] = 0
             else:
                 self.rhs.value = np.zeros_like(self.Ca.value)
